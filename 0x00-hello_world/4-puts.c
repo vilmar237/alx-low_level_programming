8669e055cde6e98e1afcaf6916665ad9f965e39f
vilmar237 <djilovilmar@gmail.com>
2022-07-05 15:05:01 -0700

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,12 @@
 /**
  *  * main - Entry point
  *   *
- *    * Return:always 0 
+ *    * Return: Always 0 (Success)
  *     */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
-
