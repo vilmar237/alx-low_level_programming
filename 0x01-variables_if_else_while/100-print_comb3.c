d7b4f349e967919e3abb32df7ae663ee9c4a8b9b
vilmar237 <djilovilmar@gmail.com>
2022-07-07 05:52:47 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 /**
- *   * main - Prints the numbers from 00 to 99
+ *   * main - Prints combination of numbers
  *     *
  *       * Return: Always (Success)
 */
 int main(void)
 {
 int c, i;
-
-c = i = '0';
 for (c = '0'; c <= '9'; c++)
-{
+{								
 for (i = '0'; i <= '9'; i++)
 {
-if ((c != i) && (c < i))
+if (c < i)
 {
 putchar(c);
 putchar(i);
@@ -24,6 +22,6 @@ putchar(' ');
 }
 }
 }
+}
 return (0);
 }
-
