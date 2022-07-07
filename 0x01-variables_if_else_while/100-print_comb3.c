f9f7a768d308512567a8444adadd7963a2803aad
vilmar237 <djilovilmar@gmail.com>
2022-07-07 05:22:14 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,11 +14,10 @@ for (c = '0'; c < '9'; c++)
 {
 for (i = '1'; i <= '9'; i++)
 {		
-
+if(c !=i)
+{
 putchar(c);
 putchar(i);
-if ((c != '9') || (c == '9' && i != '9'))
-{
 putchar(',');
 putchar(' ');
 }
