bc8fab38d5749b10c4f0da8f0d996c4693a3efd2
vilmar237 <djilovilmar@gmail.com>
2022-07-07 04:35:10 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,12 @@ int c;
 for (c = '0'; c <= '9'; c++)
 {
 putchar(c);
-										if (c != '9')									{
-										putchar(',');
-										putchar(' ');									}
+if (c !='9')							
+{putchar(',');
+putchar(' ');									
 }
-putchar('\n');
+}
+putchar('\n')
 
 return (0);
 }
