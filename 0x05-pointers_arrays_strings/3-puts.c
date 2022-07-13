93a293ce12d433e1026ff87bcfb2aff307d5e95e
vilmar237 <djilovilmar@gmail.com>
2022-07-13 15:17:06 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - is the function that print char by char of a string
+ * *str: is the parameter of a function _puts
+ * Return: always 0
+*/
+
+void _puts(char *str)
+{
+for (; *str !='\0'; str++)
+{
+_putchar(*str);
+}
+ _putchar('\n');
+}
+
