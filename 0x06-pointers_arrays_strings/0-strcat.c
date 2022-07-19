494cc349d9f4773d47c4ec63f9d3c57addb249fa
vilmar237 <djilovilmar@gmail.com>
2022-07-19 08:01:54 -0700

    tast 0 concatenate two string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination of string
+ * @src: source of string 
+ *
+ * Return: a pointer to the resulting string dest
+*/
+char *_strcat(char *dest, char *src)
+{
+char *s = dest;
+while (*dest != '\0')
+{
+dest++;
+}
+while(*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest ='\0';
+return (s);
+}
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is the readme of pointers arrays strings
