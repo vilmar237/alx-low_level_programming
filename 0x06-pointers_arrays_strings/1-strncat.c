3ee344ba4cfe683e789f2e59cdaebfcc913ef5bc
vilmar237 <djilovilmar@gmail.com>
2022-07-20 07:41:37 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,11 +10,14 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
+
 int i, j;
+
 for (i = 0; dest[i] != '\0'; i++)
 {
 continue;
 }
+
 for (j = 0; src[j] != '\0' && j < n; j++)
 {
 dest[i + j] = src[j];
