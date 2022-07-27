e00095b49197017051cf5c08a2870ca5ea6ab659
vilmar237 <djilovilmar@gmail.com>
2022-07-27 09:16:54 -0700

    libmy.a

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,22 +1,18 @@
-#include "main.h"                                                                                                                               
-                                                                                                                                                
-/**                                                                                                                                             
- *  *   * main - Entry point                                                                                                                       
- *   *     *                                                                                                                                        
- *    *       * Return: Always 0                                                                                                                     
-*/                                                                                                                                              
-int main(void)                                                                                                                                  
-{                                                                                                                                               
-_putchar('_');                                                                                                                                  
-_putchar('p');                                                                                                                                  
-_putchar('u');                                                                                                                                  
-_putchar('t');                                                                                                                                  
-_putchar('c');                                                                                                                                  
-_putchar('h');                                                                                                                                  
-_putchar('a');                                                                                                                                  
-_putchar('r');                                                                                                                                  
-_putchar('\n');                                                                                                                                 
-	                                                                                                                                                
-return (0);                                                                                                                                     
-}  
+#include "main.h"
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: always 0(Success)
+*/
+int main(void)
+{
+char str[] = "_putchar";
+int ch;
+
+for (ch = 0; ch < 8; ++ch)
+_putchar(str[ch]);
+_putchar('\n');
+
+return (0);
+}
 
