45ef515b5643782cd9ac3cf88cef5fffcced5ae6
vilmar237 <djilovilmar@gmail.com>
2022-07-31 09:57:16 -0700

    print name function

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  * main - print number of arguments entered
+ *   * @argc: number of arguments
+ *    * @argv: arguments entered
+ *     * Description: print number of arguments
+ *      * Return: return zero
+*/
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+
+
+return (0);
+}
+
