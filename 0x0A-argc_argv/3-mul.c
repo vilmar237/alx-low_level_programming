e42ceadc113d4cb52d104479b34ad15961bab5f3
vilmar237 <djilovilmar@gmail.com>
2022-07-31 10:25:12 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  * main - multiplies two numbers
+ *   * @argc: argument counter
+ *    * @argv: array of pointer to strings
+ *     *
+ *      * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int i, j, k;
+
+i = 1;
+j = 2;
+k = 3;
+if (argc != k)
+{
+printf("Error\n");
+return (1);
+}	
+printf("%d\n", atoi(argv[i]) * atoi(argv[j]));
+return (0);
+}
