9c4b90367e180c42f6ae8fb56d35f4603e4f1833
vilmar237 <djilovilmar@gmail.com>
2022-07-31 09:42:59 -0700

    print name function

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,10 @@
 
 int main(int argc, char *argv[])
 {
-(void) argc;
-printf("program's name is: %s\n",argv[0]);
-return 0;
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
 }
+return (0);
 
