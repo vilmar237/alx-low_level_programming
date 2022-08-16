c58a51f873fd90479b7cff1bbe92208363acf403
vilmar237 <djilovilmar@gmail.com>
2022-08-16 12:53:49 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
 #include <stddef.h>
 /**
  *  * struct listint_s - singly linked list
@@ -29,4 +31,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
-
+#endif /*LISTS_H*/
